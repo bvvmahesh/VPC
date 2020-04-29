0a134fef2e23379259a65eec6b0214292c623f1b
BavisettyMahesh <bavisetty.veera@gmail.com>
2020-04-29 17:19:19 +0530

    new data pushed

--- module/vpc.tf
+++ module/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block = var.VPC_CIDR
   tags = {
-    Name = var.TAGS["PROJECT"]
+    Name = var.TAGS["ENV"]
   }
   //tags       = var.TAGS["ENV"]
 }
--- prod/terraform.tfvars
+++ prod/terraform.tfvars
@@ -1,3 +1,3 @@
 VPC_CIDR = "10.100.0.0/16"
-TAGS     =  {"PROJECT" = "roboshop", "ENV"="prod"}
+TAGS     =  {"PROJECT" = "roboshop", "ENV"="Prod"}
 //TAGS     =  {"ENV"="prod"}
