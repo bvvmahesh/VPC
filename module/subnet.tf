29b84b8113c6c104139214630f2767c9191aa7b3
BavisettyMahesh <bavisetty.veera@gmail.com>
2020-04-29 19:34:58 +0530

    new data pushed

--- module/subnet.tf
+++ module/subnet.tf
@@ -1,8 +1,9 @@
 resource "aws_subnet" "main" {
-  count         = length(data.aws_availability_zones.available.names)
-  vpc_id        = aws_vpc.main.id
-  cidr_block    =  cidrsubnet("10.100.0.0/16", 8, count.index)
-  tags          = {
-    Name        = "${var.TAGS["ENV"]}-subnet-${count.index+1}"
+  count                   = length(data.aws_availability_zones.available.names)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              =  cidrsubnet("10.100.0.0/16", 8, count.index)
+  map_public_ip_on_launch = true
+  tags                    = {
+    Name                  = "${var.TAGS["ENV"]}-subnet-${count.index+1}"
   }
 }
